06583003a62f5debb2f2de3af3db568c2b3d05b4
Max Melnik <maxpersonal1721@gmail.com>
2024-11-04 11:04:04 -0500

    Nav bar working

--- src/powersync/app/MainTabs.tsx
+++ src/powersync/app/MainTabs.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Dashboard from './dashboard'; // Your Home screen component
-import AddDevice from './addDevice'; // Another screen component
+import { Ionicons } from '@expo/vector-icons';
+import Dashboard from './dashboard';
+import AddDevice from './addDevice';
 
 const Tab = createBottomTabNavigator();
 
 function AppNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Dashboard} options={{ tabBarLabel: 'Home' }} />
-      <Tab.Screen name="Devices" component={AddDevice} options={{ tabBarLabel: 'Devices' }} />
-      {/* Add more screens here */}
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: { backgroundColor: '#313131' }, // Change background color here
+        tabBarActiveTintColor: '#ffffff', // Active icon color
+        tabBarInactiveTintColor: '#a9a9a9', // Inactive icon color
+      }}
+    >
+      <Tab.Screen 
+        name="Home" 
+        component={Dashboard} 
+        options={{ 
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" color={color} size={size} />
+          ),
+        }} 
+      />
+      <Tab.Screen 
+        name="Devices" 
+        component={AddDevice} 
+        options={{ 
+          tabBarLabel: 'Devices',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="tablet-portrait-outline" color={color} size={size} />
+          ),
+        }} 
+      />
+      {/* Add more screens with icons as needed */}
     </Tab.Navigator>
   );
 }
