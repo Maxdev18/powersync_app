ebe49d43fb47d7ab767d54a72d6fca9d37ad888c
KevVvTheDev <kevintran11082002@gmail.com>
2024-11-20 10:27:29 -0500

    hallelujah

--- src/powersync/app/profile.tsx
+++ src/powersync/app/profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+  import React, { useState } from 'react';
 import { View, Text, TextInput, Switch, Button, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from '../styles/profilePage'
