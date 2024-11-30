2a081f61e43b75eb1ee0a89ed213300842eb88f4
KevVvTheDev <kevintran11082002@gmail.com>
2024-11-29 20:30:55 -0500

    fix issues after merge

--- src/powersync/components/AddDevice.tsx
+++ src/powersync/components/AddDevice.tsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import styles from '../styles/overviewPage'
-
+import React,{ useContext } from 'react';
+import { View, Text } from 'react-native';
+import createStyles from '../styles/overviewPage'
+import themeContext from '../theme/themeContext';
 interface AddDeviceProps {
   bgColor: string;
   name: string;
   power: number;
 }
 
-const AddDevice: React.FC<AddDeviceProps> = ({ icon, name, power }) => {
+const AddDevice: React.FC<AddDeviceProps> = ({name, power, bgColor }) => {
 
   const theme = useContext(themeContext); // get the theme from the context
   const styles = createStyles(theme);
-const AddDevice: React.FC<AddDeviceProps> = ({ name, power, bgColor }) => {
+// const AddDevice: React.FC<AddDeviceProps> = ({ name, power, bgColor }) => {
   return (
     <View style={styles.biggestEaterContainer}>
       <View style={{...styles.groupItem1, backgroundColor: bgColor }}>
-        <Text style={styles.groupItem1Text}>{name[0].toUpperCase()}</Text>
+      <Text style={styles.groupItem1Text}>{name[0].toUpperCase()}</Text>
       </View>
       <View style={styles.groupItem2}>
         <Text style={styles.usedMostName}>{name}</Text>
